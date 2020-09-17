a7a9b3d0f5a9f13614b766ba6653cb92915c117c
Matt Loberg <loberg.matt@gmail.com>
2020-09-16 20:40:28 -0500

    feat(db): add logging to prisma

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,31 @@
 import { PrismaClient } from '@prisma/client';
 
+import logger from './logger';
+
 const client = new PrismaClient({
-    // TODO: setup logging
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'event',
+            level: 'info',
+        },
+        {
+            emit: 'event',
+            level: 'warn',
+        },
+        {
+            emit: 'event',
+            level: 'error',
+        },
+    ],
 });
+
+client.$on('query', (query) => logger.debug(query));
+client.$on('info', (log) => logger.info(log));
+client.$on('warn', (log) => logger.warn(log));
+client.$on('error', (log) => logger.error(log));
 
 export default client;
