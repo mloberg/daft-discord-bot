875c417023912bd501dbf5f5735d907c4b8b46f5
Matt Loberg <loberg.matt@gmail.com>
2022-02-08 19:06:12 -0600

    feat(dgc): handle errors when interaction has responded
    If an interaction has already responded, it cannot re-respond. For the error handler, check if
    responded already and update the message if it has.
    
    Allow the error handler to be customized.

--- pkg/dgc/main.go
+++ pkg/dgc/main.go
@@ -7,16 +7,28 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// Commander represents Discord slash commands
-type Commander struct {
-	commands map[string]*Command
-}
+type (
+	// ErrorHandler represents an error handler function
+	ErrorHandler func(error, *discordgo.Session, *discordgo.InteractionCreate) error
+
+	// Commander represents Discord slash commands
+	Commander struct {
+		errorHandler ErrorHandler
+		commands     map[string]*Command
+	}
+)
 
 // NewCommander returns a new command collection
 func NewCommander() *Commander {
 	return &Commander{
-		commands: map[string]*Command{},
+		errorHandler: DefaultErrorHandler,
+		commands:     map[string]*Command{},
 	}
+}
+
+// OnError sets the error handler when an error is returned from a command
+func (m *Commander) OnError(h ErrorHandler) {
+	m.errorHandler = h
 }
 
 // AddCommand adds a new slash command
@@ -27,25 +39,54 @@ func (m *Commander) AddCommand(c *Command) {
 // Handler handles interaction create events from Discord
 func (m *Commander) Handler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	name := i.ApplicationCommandData().Name
-	log.Debug().
+	l := log.With().
+		Str("interaction", i.ID).
 		Str("command", name).
 		Str("guild", i.GuildID).
 		Str("channel", i.ChannelID).
 		Str("user", GetUser(i).String()).
-		Msg("Interaction created")
+		Logger()
+	l.Debug().Msg("Interaction created")
 
 	if c, ok := m.commands[name]; ok {
 		if err := c.Run(s, i); err != nil {
-			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: fmt.Sprintf("An error occurred: %v", err),
-					Flags:   InteractionResponseEphemeral,
-				},
-			})
-			if err != nil {
-				log.Error().Err(err).Str("command", name).Msg("Could not respond to interaction")
+			l.Error().Err(err).Msg("")
+			if err := m.errorHandler(err, s, i); err != nil {
+				l.Error().Err(err).Msg("Could not respond to interaction")
 			}
 		}
 	}
+}
+
+// DefaultErrorHandler is the default command error handler
+func DefaultErrorHandler(err error, s *discordgo.Session, i *discordgo.InteractionCreate) error {
+	msg := fmt.Sprintf("An error occurred: %v", err)
+
+	if Responded(s, i) {
+		_, err = s.InteractionResponseEdit(s.State.User.ID, i.Interaction, &discordgo.WebhookEdit{
+			Content: msg,
+		})
+		return err
+	}
+
+	return s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: msg,
+			Flags:   InteractionResponseEphemeral,
+		},
+	})
+}
+
+// Responded checks if the current interaction has been responded to
+func Responded(s *discordgo.Session, i *discordgo.InteractionCreate) bool {
+	msg, err := s.InteractionResponse(s.State.User.ID, i.Interaction)
+	if err != nil {
+		// a 404 is an expected error, so ignore those, but log everything else
+		if r, ok := err.(*discordgo.RESTError); !ok || r.Response.StatusCode != 404 {
+			log.Error().Err(err).Str("interaction", i.ID).Msg("Could not fetch interaction response")
+		}
+		return false
+	}
+	return msg.ID != ""
 }
